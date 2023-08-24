aa4b3ef2a3e6e45186b7a39d79a4bf990f9848ef
Luis <luis.amez.vitoria@gmail.com>
2023-08-24 16:21:12 +0100

    Add support for testing asynchronous functions
    The test function is now an asynchronous function that awaits for the callback to revolve. That way, the tests will also work with asyncrhonous functions.

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,5 +1,6 @@
-import { expect, test } from "./testLib";
 import { average, sum } from "./stats";
+import * as statsAsync from "./statsAsync";
+import { expect, test } from "./testLib";
 
 test("sum returns the sum of all the elements of the array", () => {
     const result = sum([1, 2, 3]);
@@ -11,7 +12,21 @@ test("sum returns the sum of all the elements of the array", () => {
 test("average returns the average of all the elements of the array", () => {
     const result = average([1, 2, 3]);
     const expected = 2;
-    
+
+    expect(expected).toBe(result);
+})
+
+test("sum (async) returns the sum of all the elements of the array", async () => {
+    const result = await statsAsync.sum([1, 2, 3]);
+    const expected = 6;
+
+    expect(expected).toBe(result);
+})
+
+test("average (async) returns the average of all the elements of the array", async () => {
+    const result = await statsAsync.average([1, 2, 3]);
+    const expected = 2;
+
     expect(expected).toBe(result);
 })
 
